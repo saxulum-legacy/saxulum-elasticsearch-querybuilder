2727f326659e19d1e5d7b014b0fae852c499346a
Dominik Zogg <dominik.zogg@gmail.com>
2017-04-26 08:49:47 +0200

    replace all standalone node tests with NodeTest

--- tests/NodeTest.php
+++ tests/NodeTest.php
@@ -5,13 +5,22 @@ declare(strict_types=1);
 namespace Saxulum\Tests\ElasticSearchQueryBuilder;
 
 use Saxulum\ElasticSearchQueryBuilder\Node\ArrayNode;
+use Saxulum\ElasticSearchQueryBuilder\Node\BoolNode;
+use Saxulum\ElasticSearchQueryBuilder\Node\FloatNode;
 use Saxulum\ElasticSearchQueryBuilder\Node\IntNode;
+use Saxulum\ElasticSearchQueryBuilder\Node\NullNode;
 use Saxulum\ElasticSearchQueryBuilder\Node\ObjectNode;
 use Saxulum\ElasticSearchQueryBuilder\Node\StringNode;
-use Saxulum\ElasticSearchQueryBuilder\QueryBuilder;
 
 /**
- * @coversNothing
+ * @covers \Saxulum\ElasticSearchQueryBuilder\Node\AbstractNode
+ * @covers \Saxulum\ElasticSearchQueryBuilder\Node\ArrayNode
+ * @covers \Saxulum\ElasticSearchQueryBuilder\Node\BoolNode
+ * @covers \Saxulum\ElasticSearchQueryBuilder\Node\FloatNode
+ * @covers \Saxulum\ElasticSearchQueryBuilder\Node\IntNode
+ * @covers \Saxulum\ElasticSearchQueryBuilder\Node\NullNode
+ * @covers \Saxulum\ElasticSearchQueryBuilder\Node\ObjectNode
+ * @covers \Saxulum\ElasticSearchQueryBuilder\Node\StringNode
  */
 class NodeTest extends \PHPUnit_Framework_TestCase
 {
@@ -121,120 +130,122 @@ class NodeTest extends \PHPUnit_Framework_TestCase
 
     public function testRegexp()
     {
-        $qb = new QueryBuilder();
-        $qb
-            ->add('query', $qb->objectNode())
-                ->add('regexp', $qb->objectNode())
-                    ->add('title', $qb->stringNode('search$'))
-        ;
+        $node = ObjectNode::create()
+            ->add('query', ObjectNode::create()
+                ->add('regexp', ObjectNode::create()
+                    ->add('title', StringNode::create('search$'))
+                )
+            );
 
-        self::assertSame('{"query":{"regexp":{"title":"search$"}}}', $qb->json());
+        self::assertSame('{"query":{"regexp":{"title":"search$"}}}', $node->json());
     }
 
     public function testFuzzy()
     {
-        $qb = new QueryBuilder();
-        $qb
-            ->add('query', $qb->objectNode())
-                ->add('fuzzy', $qb->objectNode())
-                    ->add('title', $qb->objectNode())
-                        ->add('value', $qb->stringNode('sea'))
-                        ->add('fuzziness', $qb->intNode(2))
-        ;
+        $node = ObjectNode::create()
+            ->add('query', ObjectNode::create()
+                ->add('fuzzy', ObjectNode::create()
+                    ->add('title', ObjectNode::create()
+                        ->add('value', StringNode::create('sea'))
+                        ->add('fuzziness', IntNode::create(2))
+                    )
+                )
+            );
 
-        self::assertSame('{"query":{"fuzzy":{"title":{"value":"sea","fuzziness":2}}}}', $qb->json());
+        self::assertSame('{"query":{"fuzzy":{"title":{"value":"sea","fuzziness":2}}}}', $node->json());
     }
 
     public function testType()
     {
-        $qb = new QueryBuilder();
-        $qb
-            ->add('query', $qb->objectNode())
-                ->add('type', $qb->objectNode())
-                    ->add('value', $qb->stringNode('product'))
-        ;
+        $node = ObjectNode::create()
+            ->add('query', ObjectNode::create()
+                ->add('type', ObjectNode::create()
+                    ->add('value', StringNode::create('product'))
+                )
+            );
 
-        self::assertSame('{"query":{"type":{"value":"product"}}}', $qb->json());
+        self::assertSame('{"query":{"type":{"value":"product"}}}', $node->json());
     }
 
     public function testIds()
     {
-        $qb = new QueryBuilder();
-        $qb
-            ->add('query', $qb->objectNode())
-                ->add('ids', $qb->objectNode())
-                    ->add('type', $qb->stringNode('product'))
-                    ->add('values', $qb->arrayNode())
-                        ->add($qb->intNode(1))
-                        ->add($qb->intNode(2))
-        ;
+        $node = ObjectNode::create()
+            ->add('query', ObjectNode::create()
+                ->add('ids', ObjectNode::create()
+                    ->add('type', StringNode::create('product'))
+                    ->add('values', ArrayNode::create()
+                        ->add(IntNode::create(1))
+                        ->add(IntNode::create(2))
+                    )
+                )
+            );
 
-        self::assertSame('{"query":{"ids":{"type":"product","values":[1,2]}}}', $qb->json());
+        self::assertSame('{"query":{"ids":{"type":"product","values":[1,2]}}}', $node->json());
     }
 
     public function testBoolTerm()
     {
-        $qb = new QueryBuilder();
-        $qb
-            ->add('query', $qb->objectNode())
-                ->add('term', $qb->objectNode())
-                    ->add('is_published', $qb->boolNode(true))
-        ;
+        $node = ObjectNode::create()
+            ->add('query', ObjectNode::create()
+                ->add('term', ObjectNode::create()
+                    ->add('is_published', BoolNode::create(true))
+                )
+            );
 
-        self::assertSame('{"query":{"term":{"is_published":true}}}', $qb->json());
+        self::assertSame('{"query":{"term":{"is_published":true}}}', $node->json());
     }
 
     public function testNullNode()
     {
-        $qb = new QueryBuilder();
-        $qb
-            ->add('query', $qb->objectNode())
-                ->add('term', $qb->objectNode())
-                    ->add('field', $qb->nullNode())
-        ;
+        $node = ObjectNode::create()
+            ->add('query', ObjectNode::create()
+                ->add('term', ObjectNode::create()
+                    ->add('field', NullNode::create())
+                )
+            );
 
-        self::assertSame('{"query":{"term":{"field":null}}}', $qb->json());
+        self::assertSame('{"query":{"term":{"field":null}}}', $node->json());
     }
 
     public function testComplex()
     {
-        $qb = new QueryBuilder();
-        $qb
-            ->add('query', $qb->objectNode())
-                ->add('bool', $qb->objectNode())
-                    ->add('must', $qb->objectNode())
-                        ->add('term', $qb->objectNode())
-                            ->add('user', $qb->stringNode('kimchy'))
-                        ->end()
-                    ->end()
-                    ->add('filter', $qb->objectNode())
-                        ->add('term', $qb->objectNode())
-                            ->add('tag', $qb->stringNode('tech'))
-                        ->end()
-                    ->end()
-                    ->add('must_not', $qb->objectNode())
-                        ->add('range', $qb->objectNode())
-                            ->add('age', $qb->objectNode())
-                                ->add('from', $qb->intNode(10))
-                                ->add('to', $qb->intNode(20))
-                            ->end()
-                        ->end()
-                    ->end()
-                    ->add('should', $qb->arrayNode())
-                        ->add($qb->objectNode())
-                            ->add('term', $qb->objectNode())
-                                ->add('tag', $qb->stringNode('wow'))
-                            ->end()
-                        ->end()
-                        ->add($qb->objectNode())
-                            ->add('term', $qb->objectNode())
-                                ->add('tag', $qb->stringNode('elasticsearch'))
-                            ->end()
-                        ->end()
-                    ->end()
-                    ->add('minimum_should_match', $qb->intNode(1))
-                    ->add('boost', $qb->floatNode(1.1))
-        ;
+        $node = ObjectNode::create()
+            ->add('query', ObjectNode::create()
+                ->add('bool', ObjectNode::create()
+                    ->add('must', ObjectNode::create()
+                        ->add('term', ObjectNode::create()
+                            ->add('user', StringNode::create('kimchy'))
+                        )
+                    )
+                    ->add('filter', ObjectNode::create()
+                        ->add('term', ObjectNode::create()
+                            ->add('tag', StringNode::create('tech'))
+                        )
+                    )
+                    ->add('must_not', ObjectNode::create()
+                        ->add('range', ObjectNode::create()
+                            ->add('age', ObjectNode::create()
+                                ->add('from', IntNode::create(10))
+                                ->add('to', IntNode::create(20))
+                            )
+                        )
+                    )
+                    ->add('should', ArrayNode::create()
+                        ->add(ObjectNode::create()
+                            ->add('term', ObjectNode::create()
+                                ->add('tag', StringNode::create('wow'))
+                            )
+                        )
+                        ->add(ObjectNode::create()
+                            ->add('term', ObjectNode::create()
+                                ->add('tag', StringNode::create('elasticsearch'))
+                            )
+                        )
+                    )
+                    ->add('minimum_should_match', IntNode::create(1))
+                    ->add('boost', FloatNode::create(1.1))
+                )
+            );
 
         $expected = <<<EOD
 {
@@ -277,54 +288,58 @@ class NodeTest extends \PHPUnit_Framework_TestCase
 }
 EOD;
 
-        self::assertSame($expected, $qb->json(true));
+        self::assertSame($expected, $node->json(true));
     }
 
     public function testEmptyQuery()
     {
-        $qb = new QueryBuilder();
-        $qb
-            ->add('query', $qb->objectNode())
-                ->add('bool', $qb->objectNode())
-                    ->add('must', $qb->arrayNode())
-                        ->add($qb->objectNode())
-                            ->add('terms', $qb->objectNode())
-                                ->add('field', $qb->arrayNode())
-                                    ->add($qb->stringNode(null))
+        $node = ObjectNode::create()
+            ->add('query', ObjectNode::create()
+                ->add('bool', ObjectNode::create()
+                    ->add('must', ArrayNode::create()
+                        ->add(ObjectNode::create()
+                            ->add('terms', ObjectNode::create()
+                                ->add('field', ArrayNode::create()
+                                    ->add(StringNode::create())
+                                )
+                            )
+                        )
+                    )
+                )
+            );
         ;
 
-        self::assertSame('', $qb->json());
+        self::assertSame('', $node->json());
     }
 
-    /**
-     * @expectedException \Exception
-     * @expectedExceptionMessage You cannot call addToArrayNode on node type: Saxulum\ElasticSearchQueryBuilder\Node\ObjectNode
-     */
-    public function testAddToArrayNodeIfItsNotAnActiveArrayNode()
+    public function testDuplicateKeyOnObjectNode()
     {
-        $qb = new QueryBuilder();
-        $qb->addToArrayNode($qb->stringNode());
+        self::expectException(\InvalidArgumentException::class);
+        self::expectExceptionMessage('There is already a node with key name!');
+
+        ObjectNode::create()
+            ->add('name', StringNode::create())
+            ->add('name', StringNode::create());
     }
 
-    /**
-     * @expectedException \Exception
-     * @expectedExceptionMessage You cannot call addToObjectNode on node type: Saxulum\ElasticSearchQueryBuilder\Node\ArrayNode
-     */
-    public function testAddToObjectNodeIfItsNotAnActiveObjectNode()
+    public function testAssignSameNodeTwice()
     {
-        $qb = new QueryBuilder();
-        $qb
-            ->addToObjectNode('key', $qb->arrayNode())
-                ->addToObjectNode('key', $qb->stringNode())
-        ;
+        self::expectException(\InvalidArgumentException::class);
+        self::expectExceptionMessage('Node already got a parent!');
+
+        $childNode = StringNode::create();
+
+        ArrayNode::create()
+            ->add($childNode)
+            ->add($childNode);
     }
 
-    /**
-     * @expectedException \Exception
-     * @expectedExceptionMessage You cannot call end on main node
-     */
-    public function testToManyEnd()
+    public function testGetParentNode()
     {
-        (new QueryBuilder())->end();
+        $childNode = StringNode::create();
+
+        $parentNode = ArrayNode::create()->add($childNode);
+
+        self::assertSame($parentNode, $childNode->getParent());
     }
 }
